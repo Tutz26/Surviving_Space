6477847c4854305288ee08650e31fbdf050100b1
Tutz26 <tutz_26@hotmai.com>
2020-03-21 22:47:14 -0700

    Added crosshair to player obj

---
+++ Assets/Scripts/CrosshairController.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CrosshairController : MonoBehaviour
+{
+        //Object declarations:
+            GameObject playerObject;
+
+        //Vector declarations:
+            Vector2 positionTracker;
+
+        //Float declarations:
+            float distanceFromPlayer;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerObject = GameObject.FindGameObjectWithTag("Player");
+
+        //Default values:
+            distanceFromPlayer = 10f;
+
+        //Adjust this object prosition from player:
+            positionTracker = new Vector2(playerObject.transform.localPosition.x, playerObject.transform.localPosition.y + distanceFromPlayer);
+            this.transform.localPosition = positionTracker;
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
