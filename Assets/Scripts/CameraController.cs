1ce32b644e85e7a310a3531766a0ca897b1e9770
Tutz26 <tutz_26@hotmai.com>
2020-03-21 23:23:00 -0700

    Created camera controllers lorgic

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+        //Object declarations:
+            GameObject playerObject;
+            PlayerController playerController;
+
+        //Vector declarations:
+            Vector2 currentPosition;
+            Vector2 newPosition;
+
+        //float declarations:
+            float cameraFollowSpeed;
+            float cameraRotationSpeed;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerObject = GameObject.FindGameObjectWithTag("Player");
+        playerController = playerObject.GetComponent<PlayerController>();
+
+        //Default values:
+            cameraFollowSpeed = 15f;
+
+    }
+
+    void FixedUpdate()
+    {
+        this.transform.position = Vector2.Lerp(currentPosition, newPosition, cameraFollowSpeed * Time.deltaTime);
+
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        currentPosition = (Vector2) this.transform.position;
+        newPosition = (Vector2) playerObject.transform.position;
+    }
+}
